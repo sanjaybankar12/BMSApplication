e9d5881b80466b4d750efbe6619eb5684deddb57
Sanjay Bankar <sanjaybankar12@gmail.com>
2016-12-12 19:05:27 +0530

    form & add user

---
+++ src/com/dbconnection/DB.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.dbconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Sanjay
+ */
+public class DB 
+{
+    Connection c;
+    public DB()
+    {
+        try{
+            Class.forName("org.apache.derby.EmbeddedDriver");
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null,"Connection to Database failed due to driver not found...!!","Error Message",JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
+    }
+    public Connection getConnection()
+    {
+        try{
+           c=DriverManager.getConnection("jdbc:mysq://localhost:1527/bmsdb","root","root");
+           return c;
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null,"Connection to Database failed...!!","Error Message",JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+            return null;
+        }
+    }
+}
